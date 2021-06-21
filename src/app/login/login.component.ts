114f291fde359f681452adbb3a4797c32029fe01
Raghesh99 <raghesh99@gmail.com>
2021-06-21 12:42:05 +0200

    form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  @ViewChild('f') loginForm!: NgForm;
+  user = {
+    email: '',
+    password: '',
+  };
+  submitted = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    this.submitted = true;
+    this.user.email = this.loginForm.value.email;
+    this.user.password = this.loginForm.value.password;
+
+    this.loginForm.reset();
   }
-
 }
