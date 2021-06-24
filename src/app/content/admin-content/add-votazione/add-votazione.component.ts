e1e5663d410992c849b0503f9e59e329354851fd
Raghesh99 <raghesh99@gmail.com>
2021-06-24 18:18:27 +0200

    da sistemare qualche bug

--- src/app/content/admin-content/add-votazione/add-votazione.component.ts
+++ src/app/content/admin-content/add-votazione/add-votazione.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,8 +7,9 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./add-votazione.component.css'],
 })
 export class AddVotazioneComponent implements OnInit {
-  imagePath: any;
-  imgURL: any;
+  imagePath: any[] = [];
+  imgURL: any[] = [];
+
   message!: string;
   desc: string = '';
   votationForm: any;
@@ -20,21 +21,29 @@ export class AddVotazioneComponent implements OnInit {
       descrizione: new FormControl(''),
       dateStart: new FormControl(),
       dateEnd: new FormControl(),
-      card1: new FormControl(),
-      card2: new FormControl(),
-      card3: new FormControl(),
-      card4: new FormControl(),
-      card5: new FormControl(),
-      card6: new FormControl(),
-      card7: new FormControl(),
-      card8: new FormControl(),
+      partito1: new FormControl(),
+      partito2: new FormControl(),
+      partito3: new FormControl(),
+      partito4: new FormControl(),
+      partito5: new FormControl(),
+      partito6: new FormControl(),
+      partito7: new FormControl(),
+      partito8: new FormControl(),
+      partitoImg1: new FormControl(),
+      partitoImg2: new FormControl(),
+      partitoImg3: new FormControl(),
+      partitoImg4: new FormControl(),
+      partitoImg5: new FormControl(),
+      partitoImg6: new FormControl(),
+      partitoImg7: new FormControl(),
+      partitoImg8: new FormControl(),
       cards: new FormArray([]),
     });
   }
 
-  preview(files: any) {
+  preview(e: any, index: number): void {
+    let files = e.files;
     if (files.length === 0) return;
-
     var mimeType = files[0].type;
     if (mimeType.match(/image\/*/) == null) {
       this.message = 'Only images are supported.';
@@ -42,12 +51,11 @@ export class AddVotazioneComponent implements OnInit {
     }
 
     var reader = new FileReader();
-    this.imagePath = files;
+    this.imagePath.splice(index - 1, 1, files);
     reader.readAsDataURL(files[0]);
     reader.onload = (_event) => {
-      this.imgURL = reader.result;
+      this.imgURL.splice(index - 1, 1, reader.result);
     };
-    console.log('src/assets/images/' + this.imagePath[0].name);
   }
 
   onAddCard() {
@@ -64,6 +72,9 @@ export class AddVotazioneComponent implements OnInit {
 
   onSubmit() {
     console.log(this.votationForm);
-    this.votationForm.reset();
+    /*     this.votationForm.reset();
+    this.votationForm.patchValue({
+      descrizione: '',
+    }); */
   }
 }
